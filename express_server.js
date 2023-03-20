955a3db948d942289992d01045bc856b938e3cf7
Nazia Farooq <lapatissiere@yahoo.com>
2023-03-20 12:59:46 -0400

    url shotener created

--- express_server.js
+++ express_server.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
 };
+
+function generateRandomString(len, arr) {
+  let ans = "";
+  for (let i = len; i > 0; i--) {
+    ans += arr[Math.floor(Math.random() * arr.length)];
+  }
+  return ans;
+}
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -30,7 +38,16 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: "www.lighthouselabs.com" };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
