be7fa3134ceaa6de00214d7858abe1251d3f1632
Nazia Farooq <lapatissiere@yahoo.com>
2023-03-27 14:46:52 -0400

    cookie parser added

--- express_server.js
+++ express_server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const cookieParser = require("cookie-parser");
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+app.use(cookieParser());
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
@@ -80,16 +82,19 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase , username: req.cookies["username"],};
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"],
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: "www.lighthouselabs.com" };
+  const templateVars = { id: req.params.id, longURL: "www.lighthouselabs.com" , username: req.cookies["username"]};
   res.render("urls_show", templateVars);
 });
 
@@ -120,6 +125,6 @@ app.post("/urls/:id", (req, res) => {
 }); 
 
 app.post("/login", (req, res) => {
-  res.cookie("username")
+  res.cookie("username", req.body.username);
   res.redirect("/urls");
 })
